44652c03b1684e4957f6b58d8039297ae5e78d24
FreshNewStart <mike.aswad.j@gmail.com>
2026-05-25 12:31:24 +1000

    Github pages deploy

--- nature-park-prototype/astro.config.mjs
+++ nature-park-prototype/astro.config.mjs
@@ -1,5 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import githubPages from '@astrojs/github-pages';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://FreshNewStart.github.io',
+  base: '/Port-Moresby-Nature-Park',
+  integrations: [githubPages()]
+});
