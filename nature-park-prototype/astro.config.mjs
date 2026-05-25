0179f96bee5824de35a1a2b71e7b827f4acacd8d
FreshNewStart <mike.aswad.j@gmail.com>
2026-05-25 12:55:20 +1000

    Update astro.config.mjs

--- nature-park-prototype/astro.config.mjs
+++ nature-park-prototype/astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import githubPages from '@astrojs/github-pages';
 
 export default defineConfig({
   site: 'https://FreshNewStart.github.io',
-  base: '/Port-Moresby-Nature-Park',
-  integrations: [githubPages()]
+  base: '/Port-Moresby-Nature-Park'
 });
